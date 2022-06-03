ff04a10f51dc3647c85a19ae1eb0c48f65b10d49
SG-Marco <rollingpoe@gmail.com>
2022-06-03 17:14:54 +0900

    end

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CategoryModel, DrinkModel
 
 # Register your models here.
+admin.site.register(CategoryModel)
+admin.site.register(DrinkModel) 
--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import CategoryModel
 
 # Create your views here.
+def order(request):
+    if request.method == 'GET':
+        return render(request, 'order.html')
+    elif request.method == 'POST':
+        coffee_category = request.POST.get('coffee_category', '')
+        category = CategoryModel.objects.get(coffee_category)
+
+        return render(request, 'order.html')
+
